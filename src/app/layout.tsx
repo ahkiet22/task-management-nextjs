8fc34b4d19c4f2e78b501de1a3074acac5ae9862
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-03-06 01:48:39 +0700

    build: create partial header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import Logo from "../../public/images/logo.svg.png";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,45 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="container mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl bg-[#F3F8FF]">
+          <header>
+            <div className="flex items-center justify-between p-6">
+              <div>
+                <Image
+                  src={Logo}
+                  width={150}
+                  height={500}
+                  alt="logo"
+                  className="object-cover"
+                />
+              </div>
+              <div className="flex items-center justify-between min-w-[500px]">
+                <ul className="flex gap-x-4 font-bold">
+                  <li>
+                    <span>IPhone</span>
+                  </li>
+                  <li>
+                    <span>Android</span>
+                  </li>
+                  <li>
+                    <span>Help</span>
+                  </li>
+                  <li>
+                    <span>Company</span>
+                  </li>
+                </ul>
+                <div>
+                  <Link href="/login">
+                    <span className="text-sm w-auto h-[40px] bg-[#4335DE] rounded-2xl text-white px-6 py-2 hover:bg-blue-600 transition-all duration-250 ease-in-out transform hover:scale-105 shadow-lg">
+                      Sign in
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </header>
+          {children}
+        </div>
       </body>
     </html>
   );
