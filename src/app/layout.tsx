c04df6bffc96393e4a93c89dfe12375b82897003
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-03-06 02:22:34 +0700

    build: create partial footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Link from "next/link";
 import Logo from "../../public/images/logo.svg.png";
 import Image from "next/image";
+import { FaFacebookSquare, FaYoutube } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,8 +32,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="container mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl bg-[#F3F8FF]">
-          <header>
+        <header className="ng-[#F3F8FF]">
+          <div className="container mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl ">
             <div className="flex items-center justify-between p-6">
               <div>
                 <Image
@@ -66,9 +68,74 @@ export default function RootLayout({
                 </div>
               </div>
             </div>
-          </header>
+          </div>
+        </header>
+        <div className="container mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl py-8">
           {children}
         </div>
+        <footer className="bg-[#02033B] text-white">
+          <div className="container mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl py-8">
+            <div className="flex items-center justify-between">
+              <div className="flex flex-col gap-y-4">
+                <h2 className="font-bold text-2xl">Scan. Detect. Remove.</h2>
+                <ul className="flex gap-x-4 text-2xl">
+                  <li>
+                    <FaSquareXTwitter />
+                  </li>
+                  <li>
+                    <FaFacebookSquare />
+                  </li>
+                  <li>
+                    <FaYoutube />
+                  </li>
+                </ul>
+                <div className="flex gap-x-3 text-xs underline">
+                  <span>Privacy Policy</span>
+                  <span>Terms of Service</span>
+                </div>
+                <p className="text-xs">
+                  Copyright © 2022 Certo Software Limited | Registered in
+                  England & Wales No. 10072356
+                </p>
+                <p className="text-xs">
+                  Designed & developed by Bigger Picture
+                </p>
+              </div>
+              <div className="flex flex-col gap-y-2">
+                <h2 className="font-bold text-3xl">Certo</h2>
+                <div className="w-[50px] h-[1px] bg-white"></div>
+                <ul className="text-[#FFC247] flex flex-col gap-y-4">
+                  <li>iPhone</li>
+                  <li>Android</li>
+                  <li>Help</li>
+                  <li>About</li>
+                  <li>Insights</li>
+                </ul>
+              </div>
+              <div>
+                <div className="bg-[#FFC247] w-[436px] h-auto rounded-3xl px-6 py-8 flex flex-col gap-y-2">
+                  <h2 className="font-bold text-black text-2xl">
+                    Sign up to our newsletter
+                  </h2>
+                  <p className="font-bold text-black text-xs">
+                    Receive the latest mobile security news, exclusive discounts
+                    & offers straight to your inbox!
+                  </p>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Email address"
+                      className="text-gray-500 bg-white rounded-l-2xl focus:outline-none px-4 focus:ring-2 focus:ring-blue-500"
+                    />
+                    <button className="bg-black px-4 rounded-r-2xl">
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   );
